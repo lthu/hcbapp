0163aca01f01e75e971dfc93c7b0d250db509497
lthu <lhuuskon@edu.lapinamk.fi>
2023-01-29 12:16:38 +0200

    Improved journey details dialog and added custom markers to stations.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,14 +13,14 @@ import * as L from 'leaflet';
      
     ></div>`
 })
-export class MapComponent implements OnInit, OnDestroy {
+export class MapComponent implements OnDestroy {
   @Output() map$: EventEmitter<Map> = new EventEmitter;
   @Output() zoom$: EventEmitter<number> = new EventEmitter;
   @Input() departureX! : number;
   @Input() departureY! : number;
   @Input() returnX! : number;
   @Input() returnY! : number;
-  @Input() options: MapOptions= {
+  @Input() options: MapOptions = {
                       layers:[tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                         opacity: 1,
                         maxZoom: 19,
@@ -32,55 +32,55 @@ export class MapComponent implements OnInit, OnDestroy {
   };
   public map!: Map;
   public zoom!: number;
-  public x!: number;
-  public y!: number;
   public mapReady: boolean = false;
-  private greenIcon;
+  private journeyFinishIcon;
   
   constructor() { 
-  const iconRetinaUrl = 'assets/marker-icon-2x.png';
-  const iconUrl = 'assets/marker-icon.png';
-  const shadowUrl = 'assets/marker-shadow.png';
-  const iconDefault = icon({
-    iconRetinaUrl,
-    iconUrl,
-    shadowUrl,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    tooltipAnchor: [16, -28],
-    shadowSize: [41, 41]
+    const iconRetinaUrl = 'assets/marker-icon-2x.png';
+    const iconUrl = 'assets/marker-icon.png';
+    const shadowUrl = 'assets/marker-shadow.png';
+    const iconDefault = icon({
+      iconRetinaUrl,
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+      });
+
+    Marker.prototype.options.icon = iconDefault;
+    this.journeyFinishIcon = L.icon({
+      iconUrl: 'assets/marker-icon_finish.png',
+      //shadowUrl: 'assets/marker-shadow.png',
+
+      iconSize:     [40, 40], // size of the icon
+      shadowSize:   [41, 41], // size of the shadow
+      iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
-  Marker.prototype.options.icon = iconDefault; 
-  this.greenIcon = L.icon({
-    iconUrl: 'assets/marker-icon_start.png',
-    shadowUrl: 'assets/marker-shadow.png',
-
-    iconSize:     [25, 41], // size of the icon
-    shadowSize:   [41, 41], // size of the shadow
-    iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
 
   }
 
-  ngOnInit() {
-    
-  }
+  // Get coordinates for departure and return stations from child component (journey-details). 
+  // Add specific markers to stations and finally pan the map to avg coordinates (center map relation to both stations)
   ngOnChanges(changes: SimpleChanges) {
     if (this.mapReady) {
-      
       this.departureX = changes['departureX'].currentValue
       this.departureY = changes['departureY'].currentValue
       this.returnX = changes['returnX'].currentValue
       this.returnY = changes['returnY'].currentValue
+
       L.marker([this.departureY, this.departureX]).addTo(this.map);
-      L.marker([this.returnY, this.returnX],{icon: this.greenIcon}).addTo(this.map);
+      L.marker([this.returnY, this.returnX],{icon: this.journeyFinishIcon}).addTo(this.map);
+      
+      var x_avg = (this.departureX + this.returnX) / 2
+      var y_avg = (this.departureY + this.returnY) / 2
+      this.map.panTo(new L.LatLng(y_avg, x_avg));
       
     }
-    
-    
     
   }
  
@@ -96,8 +96,7 @@ export class MapComponent implements OnInit, OnDestroy {
     this.zoom = map.getZoom();
     this.zoom$.emit(this.zoom);
     this.mapReady = true;
-    
-    
+
     
     
   }
--- src/app/view/journey-details.component.ts
+++ src/app/view/journey-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges, Inject, OnInit } from '@angular/core';
 import { JourneyDetails } from '../model/journey';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DataService } from '../data.service';
 import { Map } from 'leaflet';
 import * as L from 'leaflet';
@@ -8,10 +8,14 @@ import * as L from 'leaflet';
 @Component({
   selector: 'app-journey-details',
   styleUrls: ['./journey-details.component.css'],
-  template: `<div class="fs-container"> <p class="journeyDetails">Journey details {{journeyDetails.id}} {{journeyDetails.departure_station_name}} -> {{journeyDetails.return_station_name}}</p>
-  <p>{{journeyDetails.departure_station_coordinate_x}} , {{journeyDetails.departure_station_coordinate_y}} </p>
+  template: `<div class="fs-container"> 
+    <h2 class="journeyDetails">Journey details</h2>
+    <p class="journeyDetails">From {{journeyDetails.departure_station_name}} to {{journeyDetails.return_station_name}}</p>
+    <p class="journeyDetails">Starting {{journeyDetails.departure_time | date: 'd.L.y H:mm:ss' }} until {{journeyDetails.return_time | date: 'd.L.y H:mm:ss'}}</p>
+    <p class="journeyDetails">Distance {{journeyDetails.distance/1000}} km  Duration {{journeyDetails.duration | number: '1.0-0' }} minutes</p>
+    
+  
 
-  <p>{{journeyDetails.return_station_coordinate_x}} , {{journeyDetails.return_station_coordinate_y}} </p>
   
     <app-map 
     (map$)="receiveMap($event)"
@@ -31,6 +35,7 @@ export class JourneyDetailsComponent implements OnChanges, OnInit {
   journeyDetails: JourneyDetails = {id: 0, departure_time: "", return_time: "", departure_station_name: "", return_station_name: "", distance: 0, duration: 0, departure_station_coordinate_x: 0, departure_station_coordinate_y: 0, return_station_coordinate_x: 0, return_station_coordinate_y: 0 }
   test: number = 1;
   testBoolean: boolean = false;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ds: DataService ) {}
   
   // On initialization fetch single journey details. Data id is received from parent component.
@@ -43,7 +48,10 @@ export class JourneyDetailsComponent implements OnChanges, OnInit {
       
     })
   }
-    
+  
+  formatDuration() {
+
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     // console.log(changes['journeyDetails'].currentValue.departure_station_coordinate_x)
